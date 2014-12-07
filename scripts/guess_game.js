7cbb1b72fc271539546820380cf1a729d915b1a2
Richard Moore <rmoore424@gmail.com>
2014-12-06 22:16:12 -0500

    Added colors to determine how far away the number is

---
+++ scripts/guess_game.js
@@ -0,0 +1,21 @@
+var rightNumber = Math.floor(Math.random() *100) + 1;
+
+function setNumber() {
+	var value = $('.answer').val();
+	$(".number").each(function(idx) {
+		if (value == idx+1) {
+			if (value > rightNumber+30 || value < rightNumber-30) {
+				$(this).css("background-color", "#1A1FD9")
+			}
+			else if ((value > rightNumber+15 && value <=rightNumber+30) || (value < rightNumber-15 && value >=rightNumber-30)){
+				$(this).css("background-color", "#80B8C4");
+			}
+			else if ((value > rightNumber+5 && value <=rightNumber+15) || (value < rightNumber-5 && value >=rightNumber-15)){
+				$(this).css("background-color", "#FF6B78");
+			}
+			else {
+				$(this).css("background-color", "#D22018")
+			}
+		}   
+	})
+}
